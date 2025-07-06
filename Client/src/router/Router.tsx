a6cef8694a90c345d6d5e6b09b89120709a0ee69
Mehedi <mehedihasan67705251@gmail.com>
2025-07-07 02:02:51 +0600

    feat: add authentication routes and schemas for user registration, login, password reset, and email verification

--- Client/src/router/Router.tsx
+++ Client/src/router/Router.tsx
@@ -3,6 +3,9 @@ import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
+import ForgotPassword from "../pages/Auth/ForgotPassword";
+import ResetPassword from "../pages/Auth/ResetPassword";
+import VerifyEmail from "../pages/Auth/VerifyEmail";
 
 export const router = createBrowserRouter([
     {
@@ -20,6 +23,18 @@ export const router = createBrowserRouter([
             {
                 path: "register",
                 element: <Register></Register>,
+            },
+            {
+                path: "forgot-password",
+                element: <ForgotPassword></ForgotPassword>,
+            },
+            {
+                path: "reset-password",
+                element: <ResetPassword></ResetPassword>,
+            },
+            {
+                path: "verify-email",
+                element: <VerifyEmail></VerifyEmail>,
             },
         ],
     },
