ebf039865040c1e72917eaa86d7e2e52e7eda218
Mehedi <mehedihasan67705251@gmail.com>
2025-07-07 16:48:13 +0600

    feat: wrap main application in AuthInitializer for authentication management

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Navbar from "./pages/shared/Navbar";
 import Footer from './pages/shared/Footer';
+import AuthInitializer from './components/AuthInitializer';
 import { Outlet } from "react-router";
 
 function App() {
     return (
-        <div>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
-        </div>
+        <AuthInitializer>
+            <div>
+                <Navbar/>
+                <Outlet/>
+                <Footer/>
+            </div>
+        </AuthInitializer>
     );
 }
 
