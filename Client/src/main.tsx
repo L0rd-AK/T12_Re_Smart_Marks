a480f614924debab6f07e64ba378b49162ca091d
Mehedi <mehedihasan67705251@gmail.com>
2025-07-07 17:27:22 +0600

    chore: add comments to clarify Google Client ID usage in main.tsx

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -9,7 +9,12 @@ import { router } from './router/Router.tsx';
 import { store, persistor } from './redux/store'
 import LoadingSpinner from './components/LoadingSpinner'
 
+
+// Ensure Google Client ID is set in environment variables
+// This should be defined in your .env file as VITE_GOOGLE_CLIENT_ID
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
