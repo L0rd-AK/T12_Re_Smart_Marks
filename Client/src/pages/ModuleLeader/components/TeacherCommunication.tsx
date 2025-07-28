3d0a6396f809c7b3a15414b4a8a30c90c4048785
l0rd-AK <aka883207@gmail.com>
2025-07-29 05:30:59 +0600

    update text color in message status filter for better visibility

--- Client/src/pages/ModuleLeader/components/TeacherCommunication.tsx
+++ Client/src/pages/ModuleLeader/components/TeacherCommunication.tsx
@@ -354,7 +354,7 @@ const TeacherCommunication: React.FC = () => {
         <select
           value={filterStatus}
           onChange={(e) => setFilterStatus(e.target.value)}
-          className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border border-gray-300 text-black rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           title="Filter by message status"
         >
           <option value="all">All Status</option>
