46e25f246f103aae2cb8c65767237648e529b04d
Mehedi <mehedihasan67705251@gmail.com>
2025-07-28 21:05:45 +0600

    feat: update Navbar links and user roles for improved navigation and access control

--- Client/src/pages/shared/Navbar.tsx
+++ Client/src/pages/shared/Navbar.tsx
@@ -11,19 +11,22 @@ const links = (user?: any) => (
         <li>
             <NavLink to="/document-submission">Document Submission</NavLink>
         </li>
-        <li>
+        {/* <li>
             <NavLink to="/document-submissions">My Submissions</NavLink>
+        </li> */}
+        <li>
+            <NavLink to="/courses">Courses</NavLink>
         </li>
         {user?.role === 'module-leader' && (
-                <li>
-                    <NavLink to="/module-leader">Module Leader</NavLink>
-                </li>
+            <li>
+                <NavLink to="/module-leader">Module Leader</NavLink>
+            </li>
         )}
         {user?.role === 'admin' && (
             <li>
                 <NavLink to="/admin">Admin Dashboard</NavLink>
             </li>
-        )} 
+        )}
     </>
 );
 
