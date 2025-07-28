d86280ba9107086ace959c960aa667315297513b
l0rd-AK <aka883207@gmail.com>
2025-07-29 05:29:38 +0600

    update text color in target audience select for better visibility

--- Client/src/pages/ModuleLeader/components/TeacherCommunication.tsx
+++ Client/src/pages/ModuleLeader/components/TeacherCommunication.tsx
@@ -638,7 +638,7 @@ const TeacherCommunication: React.FC = () => {
                   <select
                     value={newNotification.targetAudience}
                     onChange={(e) => setNewNotification(prev => ({ ...prev, targetAudience: e.target.value as Notification['targetAudience'] }))}
-                    className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="w-full border border-gray-300 text-black rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     title="Target audience"
                   >
                     <option value="teachers">Teachers</option>
