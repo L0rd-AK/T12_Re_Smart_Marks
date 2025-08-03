ed850da53a347b8eb5e1de174ae2291d1769cd47
l0rd-AK <aka883207@gmail.com>
2025-08-04 02:20:32 +0600

    update user display in ModuleLeaderDashboard to use consolidated name field

--- Client/src/pages/ModuleLeader/components/QuestionPaperTemplates.tsx
+++ Client/src/pages/ModuleLeader/components/QuestionPaperTemplates.tsx
@@ -226,7 +226,7 @@ const QuestionPaperTemplates: React.FC = () => {
         <select
           value={filterType}
           onChange={(e) => setFilterType(e.target.value)}
-          className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border text-black border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           title="Filter templates by type"
         >
           <option value="all">All Types</option>
