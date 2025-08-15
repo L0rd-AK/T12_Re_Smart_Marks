98395960f3afb8c4a00d6f515d2b98996a9d18bb
l0rd-AK <aka883207@gmail.com>
2025-08-16 05:07:26 +0600

    fix: add proper TypeScript interface for coursesApi getCourses query

--- Client/src/redux/api/coursesApi.ts
+++ Client/src/redux/api/coursesApi.ts
@@ -1,9 +1,20 @@
 import { baseApi } from "./baseApi";
 
+interface CourseResponse {
+  success: boolean;
+  data: Array<{
+    _id: string;
+    name: string;
+    code: string;
+    department: string;
+    moduleLeader?: string;
+  }>;
+}
+
 export const coursesApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
   // Courses endpoints
-    getCourses: builder.query<any, void>({
+    getCourses: builder.query<CourseResponse, void>({
       query: () => '/courses',
       providesTags: ['Course'],
     }),
